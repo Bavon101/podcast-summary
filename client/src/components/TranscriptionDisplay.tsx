fd8425115639ed75913ab785a89158915581926c
mutuawambua <joseph.mutua@sovtech.com>
2024-07-26 13:19:20 +0300

    Fixed merge conflict

--- client/src/components/TranscriptionDisplay.tsx
+++ client/src/components/TranscriptionDisplay.tsx
@@ -13,29 +13,35 @@ const TranscriptionDisplay: React.FC<TranscriptionDisplayProps> = ({
   highlights,
 }) => {
   return (
-    <div className="mt-8 w-full bg-white p-6 rounded shadow-md">
-      <h2 className="text-2xl font-bold text-gray-800">Transcription</h2>
-      <p className="mt-2 text-gray-700 whitespace-pre-wrap">
-        <Typewriter
-          words={[transcription]}
-          typeSpeed={50}
-          cursor
-          cursorStyle="_"
-        />
-      </p>
-      <h2 className="text-2xl font-bold text-gray-800 mt-4">Summary</h2>
-      <p className="mt-2 text-gray-700 whitespace-pre-wrap">
-        <Typewriter words={[summary]} typeSpeed={50} cursor cursorStyle="_" />
-      </p>
-      <h2 className="text-2xl font-bold text-gray-800 mt-4">Highlights</h2>
-      <p className="mt-2 text-gray-700 whitespace-pre-wrap">
-        <Typewriter
-          words={[highlights]}
-          typeSpeed={50}
-          cursor
-          cursorStyle="_"
-        />
-      </p>
+    <div className="mt-8 w-screen bg-white p-6 rounded shadow-md grid grid-cols-2 gap-4">
+      <div>
+        <h2 className="text-2xl font-bold text-gray-800">Transcription</h2>
+        <p className="mt-2 text-gray-700 whitespace-pre-wrap">
+          <Typewriter
+            words={[transcription]}
+            typeSpeed={50}
+            cursor
+            cursorStyle="_"
+          />
+        </p>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-gray-800">Summary</h2>
+        <p className="mt-2 text-gray-700 whitespace-pre-wrap">
+          <Typewriter words={[summary]} typeSpeed={50} cursor cursorStyle="_" />
+        </p>
+      </div>
+      <div className="col-span-2">
+        <h2 className="text-2xl font-bold text-gray-800 mt-4">Highlights</h2>
+        <p className="mt-2 text-gray-700 whitespace-pre-wrap">
+          <Typewriter
+            words={[highlights]}
+            typeSpeed={50}
+            cursor
+            cursorStyle="_"
+          />
+        </p>
+      </div>
     </div>
   );
 };
